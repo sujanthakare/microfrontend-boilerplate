e8a60446e5da80e5934e159801e986184e6e0d18
Sujan Thakare <sujan.thakare@atg.se>
2021-09-14 21:54:12 +0200

    Added new line in app.tsx

---
+++ app.tsx
@@ -0,0 +1,13 @@
+import React from 'react';
+
+const App = () => {
+  return (
+    <div>
+      <h1>WORK IN PROGRESS</h1>
+      <div>WEBPACK-BOILERPLATE</div>
+      <h3>CHECK THIS</h3>
+    </div>
+  );
+};
+
+export default App;
